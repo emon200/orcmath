19bca138b103ed89a2630931f62a7cb180d95282
BT_1N3_25 <BT_1N3_25@BT_1N3_25.bktech.local>
2017-12-18 11:44:03 -0500

    CSV maker

--- OrcMathGUI/src/playground/CatalogMaker.java
+++ OrcMathGUI/src/playground/CatalogMaker.java
@@ -1,9 +1,16 @@
 package playground;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CatalogMaker {
 	static ArrayList<Toys> running;
+	private String a;
+	private String b;
+	private String c;
+	private String d;
+
+	
 	public static void main(String[] args) {
 		CatalogMaker maker = new CatalogMaker();
 		System.out.println(maker.getContent());
@@ -11,12 +18,31 @@ public class CatalogMaker {
 
 	public CatalogMaker() {
 		running = new ArrayList<Toys>();
+		makeSuperHero();
+				}
+	public void makeSuperHero() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Create your own superhero.");
+		System.out.println("Enter the first name of your superhero");
+		a = in.nextLine();
+		System.out.println("Enter the last name of your superhero");
+		b = in.nextLine();
+		System.out.println("What's his/her alias?");
+		c = in.nextLine();
+		System.out.println("What's his/her superpower?");
+		d = in.nextLine();
+		System.out.println("Would you like to add another?");
+		String e=in.nextLine();
 
-		running.add(new Toys(1,1,1,"sprinter","jogger"));
-		running.add(new Toys(2,2,2,"walker","marathon"));
-		running.add(new Toys(3,3,3,"limping","idling"));
-		running.add(new Toys(4,4,4,"sitting","standing"));
+		if(e.toLowerCase().contains("yes")){
+			running.add(new Toys(a,b,c,d));
+			makeSuperHero();
+			}
+		else {	
+			running.add(new Toys(a,b,c,d));
 	}
+		}
+	
 	public String getContent(){
 		String data = "";
 		for(Toys t:running) {
--- OrcMathGUI/src/playground/Toys.java
+++ OrcMathGUI/src/playground/Toys.java
@@ -7,19 +7,17 @@ import guiTeacher.components.AnimatedComponent;
 import guiTeacher.components.Component;
 
 public class Toys extends AnimatedComponent{
-	private int first = 0;
-	private int second = 0;
-	private int third = 0;
-	private String why;
-	private String how;
+	private String first;
+	private String second;
+	private String third;
+	private String fourth;
 	
-	public Toys(int first, int second, int third, String why, String how) {	
+	public Toys(String first, String second, String third, String fourth) {	
 		super(40,40,160,225);
 		this.first = first;
 		this.second = second;
 		this.third = third;
-		this.why = why;
-		this.how=how;
+		this.fourth =  fourth;
 	
 		addSequence("resources/running.png", 150, 9, 67, 159, 223, 9);
 		Thread animation = new Thread(this);
@@ -33,7 +31,7 @@ public class Toys extends AnimatedComponent{
 	}
 	
 	public String toString() {
-		return first + "," + second + "," + third;
+		return first + "," + second + "," + third + "," + fourth;
 	}
 
 }
